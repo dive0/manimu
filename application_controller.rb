f1c42382765017146c9ffb75b7f64b6aa72c566c
Shi Wei Zheng <shiz3150@hstat.org>
2019-03-14 01:41:01 +0000

    get info from jikan to show in terminal

--- application_controller.rb
+++ application_controller.rb
@@ -1,5 +1,7 @@
 require 'dotenv/load'
 require 'bundler'
+require 'net/http'
+require 'json'
 Bundler.require
 
 require_relative 'models/model.rb'
@@ -7,6 +9,13 @@ require_relative 'models/model.rb'
 class ApplicationController < Sinatra::Base
 
   get '/' do
+    airing_url = 'https://api.jikan.moe/v3/top/anime/1/airing'
+    airing_uri = URI(airing_url)
+    airing_response = Net::HTTP.get(airing_uri)
+    airing_array = JSON.parse(airing_response)
+    
+    pp airing_array["top"]
+    
     erb :index
   end
   
--- models/jikan.php
+++ models/jikan.php
@@ -1,7 +1,18 @@
-<?php
-    require __DIR__ . '/vendor/autoload.php';
-    use Jikan\Jikan;
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <?php
+    // require __DIR__ . '/vendor/autoload.php';
+    // use Jikan\Jikan;
+    // use \Jikan\Helper\Constants;
+    
+    // $jikan = new Jikan;
+    // $topAnime = $jikan->TopAnime();
+    
+            echo "hello world"
+        ?>
+    </body>
 
-    $jikan = new Jikan;
-
-?>
+</html>
